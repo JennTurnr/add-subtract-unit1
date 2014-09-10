2a88b654ab25fd6c482f6752f6f629a5e76f713e
Jennifer Turner <jenniferturner202@gmail.com>
2014-09-09 23:16:58 -0400

    Add new functions

--- app.js
+++ app.js
@@ -1,65 +1,53 @@
 
-/* function add(x,y) {
-	var total = x + y;
-	return total;
-}
+function plusButton() {
+	//get the values of x and y
+	var x = parseInt($('#x').val(),10);
+	var y = parseInt($('#y').val(),10);
 
-function subtract(x,y) {
-	var total2 = x - y;
-	return total2;
-} */
+	// add the values
+	var answer =  x + y;
 
-function addItems(x,y) {
-	var addition = $('x' + 'y').val();
-	$('.answers').append('<p>'+addition+'</p>');
-	$('x' + 'y').val('');
+	// update the answer to the page
+	$('.answers').html(x + "+" + y + " is " + answer);
+
+
+	// clear the value field
+	$('#x').val('');
+	$('#y').val('');
 	event.preventDefault();
+
+	// If a non-integer is entered
+	//if (isNaN(x,y)) {
+	//	alert("Please enter a number");
+	//}
 }
 
-function subtractItems(x,y) {
-	var subtract = $('x' - 'y').val();
-	$('answers').append('<p>'+addition+'</p>');
-	$('x' + 'y').val('');
+
+function minusButton() {
+
+	// get the values of x and y
+	var x1 = parseInt($('#x').val(),10);
+	var y1 = parseInt($('#y').val(),10);
+	
+
+	// subtract the values
+	var answer2 = x1 - y1;
+
+	// update the answer to the page
+	$('.answers').html(x1 + "-" + y1 + " is " + answer2);
+
+	// clear the value field
+	$('#x').val('');
+	$('#y').val('');
 	event.preventDefault();
+
+	// If a non-integer is entered
+	//if (isNaN(x,y)) {
+		//alert("Please enter a number");
+	//}
 }
 
-$(function() {
-$('#plus').on('click', addItems);
-$('#minus').on('click', subtractItems);
 
-
-});
-
-/* 
-function addX() {
-	var getX = $('x').val();
-	$('.answers').append('<p>'+getX+'</p>');
-	$('x').val('');
-	event.preventDefault();
-}
-
-function addY() {
-	var getY=$('y').val();
-	$('answers').append('<p>'+getY+'</p>');
-	$('y').val('');
-	event.preventDefault();
-}
-
-//var addition = $('item-x' + 'item-y');
-//var subtract = $('item-x' - 'item-y');
-
-
-
-$(function() {
-$('plus').on('click', add, addX);
-$('minus').on('click', subtract, addY);
-
-
-});
-
-//function add(x, y) {
-  //  var total = x + y;
-  //  return total;
-// }
-
-*/
+// Function calls
+$('#minus').click(minusButton);
+$('#plus').click(plusButton);
